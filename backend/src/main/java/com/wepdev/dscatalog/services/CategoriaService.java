24a6509eede1c3062c18e03048a990ec961fb54a
wep1980 <wepbike@gmail.com>
2021-09-06 06:58:43 -0300

    Seeding data.sql no projeto, @Transactional(readOnly = true) evitando o locking

--- backend/src/main/java/com/wepdev/dscatalog/services/CategoriaService.java
+++ backend/src/main/java/com/wepdev/dscatalog/services/CategoriaService.java
@@ -4,6 +4,7 @@ import com.wepdev.dscatalog.entities.Categoria;
 import com.wepdev.dscatalog.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,8 +14,12 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repository;
 
-
-
+    /**
+     * Garante que o metodo vai executar em uma transaçao com o banco de dados.
+     * readOnly = true -> Evita o locking(travamento com o banco de dodos) em opereções de leitura
+     * @return
+     */
+    @Transactional(readOnly = true)
     public List<Categoria> findAll(){
         return repository.findAll();
     }
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_categoria (nome) values ('Livros');
+INSERT INTO tb_categoria (nome) values ('Eletronicos');
+INSERT INTO tb_categoria (nome) values ('Computadores');
